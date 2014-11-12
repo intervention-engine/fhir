fcbb60f2d0b8e8154d735af7aa2f51ef89b9b4e5
Andy Gregorowicz <andy@mitre.org>
2014-11-12 11:43:55 -0500

    Starting work on handling condition code filtering

--- models/query_ext.go
+++ models/query_ext.go
@@ -8,6 +8,11 @@ func (q *Query) ToPipeline() []bson.M {
 		switch extension.Url {
 		case "http://interventionengine.org/patientgender":
 			pipeline = append(pipeline, bson.M{"$match": bson.M{"gender": extension.ValueString}})
+		case "http://interventionengine.org/conditioncode":
+			// Hack for now assuming that all codable concepts contain a single code
+			conditionCode := extension.ValueCodeableConcept.Coding[0].Code
+			conditionSystem := extension.ValueCodeableConcept.Coding[0].System
+			pipeline = append(pipeline, bson.M{"$match": bson.M{"entries.type": "Condition", "entries.codes.coding.code": conditionCode, "entries.codes.coding.system": conditionSystem}})
 		}
 	}
 
