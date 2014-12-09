862df012d1c29ef104f5736870f34eb8d04c8088
Andrew Hubley <ahubley@mitre.org>
2014-12-09 13:50:51 -0500

    added check to avoid attempt at referencing type and ID from URL if url is not present or incorrect

--- models/reference_ext.go
+++ models/reference_ext.go
@@ -11,8 +11,10 @@ func (r *Reference) UnmarshalJSON(data []byte) (err error) {
 	ref := reference{}
 	if err = json.Unmarshal(data, &ref); err == nil {
 		splitURL := strings.Split(ref.Reference, "/")
-		ref.ReferencedID = splitURL[len(splitURL)-1]
-		ref.Type = splitURL[len(splitURL)-2]
+		if len(splitURL) == 3 {
+			ref.ReferencedID = splitURL[len(splitURL)-1]
+			ref.Type = splitURL[len(splitURL)-2]
+		}
 		external := strings.HasPrefix(ref.Reference, "http")
 		ref.External = &external
 		*r = Reference(ref)
