00cc4904dfa60034d9cd10f4a6be694720ee8956
Andy Gregorowicz <andy@mitre.org>
2015-03-26 14:08:27 -0400

    Adding search into medication statements

--- server/medicationstatement.go
+++ server/medicationstatement.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gorilla/context"
@@ -17,10 +18,30 @@ import (
 func MedicationStatementIndexHandler(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	var result []models.MedicationStatement
 	c := Database.C("medicationstatements")
-	iter := c.Find(nil).Limit(100).Iter()
-	err := iter.All(&result)
+	host, err := os.Hostname()
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+
+	r.ParseForm()
+	if len(r.Form) == 0 {
+		iter := c.Find(nil).Limit(100).Iter()
+		err := iter.All(&result)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+		}
+	} else {
+		for key, value := range r.Form {
+			splitKey := strings.Split(key, ":")
+			if (len(splitKey) > 1) && (splitKey[0] == "patient") {
+				subjectType := splitKey[1]
+				referenceString := "http://" + host + ":3001/" + subjectType + "/" + value[0]
+				err := c.Find(bson.M{"patient.reference": referenceString}).All(&result)
+				if err != nil {
+					http.Error(rw, err.Error(), http.StatusInternalServerError)
+				}
+			}
+		}
 	}
 
 	var medicationstatementEntryList []models.MedicationStatementBundleEntry
