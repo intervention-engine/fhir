4d7f6f25364ff9231840670e384a923f699437a4
Carlton Duffett <cjduffett@comcast.net>
2016-09-13 14:56:16 -0400

    Updated Parser and Tests
    Updated parser to handle compound indexes. Tests now thoroughly test
    the parser and mechanism for creating indexes. Pulled the updated
    index.conf file over from the generator.

--- server/mongo_indexes.go
+++ server/mongo_indexes.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,10 @@ import (
 
 	mgo "gopkg.in/mgo.v2"
 )
+
+// IndexMap is a map of index arrays with the collection name as the key. Each index array
+// contains one or more *mgo.Index indexes.
+type IndexMap map[string][]*mgo.Index
 
 // ConfigureIndexes ensures that all indexes listed in the provided indexes.conf file
 // are part of the Mongodb fhir database. If an index does not exist yet ConfigureIndexes
@@ -38,22 +43,23 @@ func ConfigureIndexes(session *mgo.Session, config Config) {
 		collection := db.C(k)
 
 		for _, index := range indexMap[k] {
-			log.Printf("Ensuring index: %s.%s: %s\n", config.DatabaseName, k, index.Key[0])
-			err = collection.EnsureIndex(index)
+			log.Printf("Ensuring index: %s.%s: %s\n", config.DatabaseName, k, sprintIndexKeys(index))
+			err = collection.EnsureIndex(*index)
 
 			if err != nil {
-				log.Printf("[WARNING] Could not ensure index: %s.%s: %s\n", config.DatabaseName, k, index.Key[0])
+				log.Printf("[WARNING] Could not ensure index: %s.%s: %s\n", config.DatabaseName, k, sprintIndexKeys(index))
 			}
 		}
 	}
 	session.Close()
 }
 
-func parseIndexes(fileContents string, configuredDBName string) (map[string][]mgo.Index, error) {
-	var indexMap = make(map[string][]mgo.Index)
+func parseIndexes(fileContents string, configuredDBName string) (IndexMap, error) {
+	var indexMap = make(IndexMap)
 	lines := strings.Split(fileContents, "\n")
 	for _, line := range lines {
 
+		// Skip blank lines or lines with bash-style comments
 		if line == "" {
 			continue
 		}
@@ -62,46 +68,138 @@ func parseIndexes(fileContents string, configuredDBName string) (map[string][]mg
 			continue
 		}
 
-		// create a new index from the next line in the config file
-		// format: <db_name>.<collection_name>.<key>_<(-)1>
-		config := strings.SplitN(line, ".", 3)
+		// Begin parsing new index from next line of file
+		// format: <db_name>.<collection_name>.<index(es)>
+		var newIndex *mgo.Index
+		var err error
 
+		config := strings.SplitN(line, ".", 3)
 		if len(config) < 3 {
-			// malformed index name
-			return nil, newParseIndexError(line, "Not of format <db_name>.<collection_name>.<key>_<(-)1>")
+			// Bad index format
+			return nil, newParseIndexError(line, "Not of format <db_name>.<collection_name>.<index(es)>")
 		}
 
 		dbName := config[0]
-
 		if dbName != configuredDBName {
-			// malformed index name
-			return nil, newParseIndexError(line, "DB name does not match server config")
+			// Wrong database name
+			return nil, newParseIndexError(line, "DB name does not match server configuration")
 		}
 
 		collectionName := config[1]
-		rest := strings.Split(config[2], "_")
-
-		if len(rest) != 2 {
-			// malformed index name
-			return nil, newParseIndexError(line, "Missing field name or index order (asc/desc)")
+		if len(collectionName) == 0 {
+			// No collection name provided
+			return nil, newParseIndexError(line, "No collection name given")
 		}
 
-		key := rest[0]
-
-		// direction determines ascending or descending order
-		direction := ""
-		if rest[1] == "-1" {
-			direction = "-"
+		indexSpec := config[2]
+		if len(indexSpec) == 0 {
+			// No index specification provided
+			return nil, newParseIndexError(line, "No index key(s) given")
 		}
 
-		indexMap[collectionName] = append(indexMap[collectionName], mgo.Index{
-			Key:        []string{fmt.Sprintf("%s%s", direction, key)},
-			Background: true, // build the index in the background; do not block other connections
-		})
+		if string(indexSpec[0]) == "(" {
+			// this is a compound index spec
+			newIndex, err = parseCompoundIndex(indexSpec)
+		} else {
+			// this is a standard index spec
+			newIndex, err = parseStandardIndex(indexSpec)
+		}
+
+		if err != nil {
+			return nil, newParseIndexError(line, err.Error())
+		}
+
+		// build the index in the background; do not block other connections
+		newIndex.Background = true
+
+		indexMap[collectionName] = append(indexMap[collectionName], newIndex)
 	}
 	return indexMap, nil
 }
 
+// parseStandardIndex parses an index of the form:
+// <db_name>.<collection_name>.<key>_(-)1
+func parseStandardIndex(indexSpec string) (*mgo.Index, error) {
+
+	key := parseIndexKey(indexSpec)
+
+	if key == "" {
+		// invalid key format, was not parsed successfully
+		return nil, errors.New("Standard key not of format: <key>_(-)1")
+	}
+
+	return &mgo.Index{
+		Key: []string{key},
+	}, nil
+}
+
+// parseCompoundIndex parses an index of the form:
+// <db_name>.<collection_name>.(<key1>_(-)1, <key2>_(-)1, ...)
+func parseCompoundIndex(indexSpec string) (*mgo.Index, error) {
+
+	// Check that the compound indexes are listed inside parentheses
+	if !strings.HasPrefix(indexSpec, "(") || !strings.HasSuffix(indexSpec, ")") {
+		return nil, errors.New("Compound key not of format: (<key1>_(-)1, <key2>_(-)1, ...)")
+	}
+
+	// Each element of specs is a standard key of the format <key>_(-)1
+	// Note: if only one key is specified in the compound format a standard (not compound) key will be returned
+	specs := strings.Split(indexSpec[1:len(indexSpec)-1], ",")
+
+	var keys []string
+
+	for _, spec := range specs {
+		key := parseIndexKey(strings.Trim(spec, " ")) // trim leading and trailing whitespace before parsing
+		if key == "" {
+			return nil, errors.New("Compound key sub-key not of format: <key>_(-)1")
+		}
+		keys = append(keys, key)
+	}
+	return &mgo.Index{
+		Key: keys,
+	}, nil
+}
+
+// parseIndexKey converts the standard mongo index key format: "<key>_(-)1"
+// to the format used by mgo.Index: "(-)<key>"
+func parseIndexKey(spec string) string {
+
+	keyAndDirection := strings.Split(spec, "_")
+
+	if len(keyAndDirection) != 2 {
+		return ""
+	}
+
+	direction := ""
+	if keyAndDirection[1] == "-1" {
+		direction = "-"
+	}
+	return fmt.Sprintf("%s%s", direction, keyAndDirection[0])
+}
+
 func newParseIndexError(indexName, reason string) error {
-	return fmt.Errorf("Index name '%s' is invalid: %s", indexName, reason)
+	return fmt.Errorf("Index '%s' is invalid: %s", indexName, reason)
+}
+
+func sprintIndexKeys(index *mgo.Index) string {
+
+	var keystr string
+	keys := index.Key
+
+	if len(keys) == 0 {
+		return ""
+	}
+
+	if len(keys) > 1 {
+		keystr = "("
+
+		for i := 0; i < len(keys)-1; i++ {
+			keystr += keys[i] + ", "
+		}
+		keystr += keys[len(keys)-1] + ")"
+	} else {
+		keystr = keys[0]
+	}
+
+	return keystr
 }
--- server/mongo_indexes_test.go
+++ server/mongo_indexes_test.go
@@ -2,10 +2,10 @@ package server
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"sort"
 	"testing"
 	"time"
 
@@ -14,6 +14,15 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/dbtest"
 )
+
+var expectedIndexes = []mgo.Index{
+	mgo.Index{Key: []string{"foo"}},
+	mgo.Index{Key: []string{"-foo"}},
+	mgo.Index{Key: []string{"bar.foo"}},
+	mgo.Index{Key: []string{"-bar.foo"}},
+	mgo.Index{Key: []string{"foo", "bar"}},
+	mgo.Index{Key: []string{"bar", "foo", "-baz"}},
+}
 
 type MongoIndexesTestSuite struct {
 	suite.Suite
@@ -24,6 +33,7 @@ type MongoIndexesTestSuite struct {
 	Database     *mgo.Database
 	Engine       *gin.Engine
 	Server       *httptest.Server
+	Config       Config
 	Interceptors map[string]InterceptorList
 	FixtureID    string
 }
@@ -33,9 +43,9 @@ func (s *MongoIndexesTestSuite) SetupSuite() {
 	s.Local, _ = time.LoadLocation("Local")
 
 	// Server configuration
-	config := DefaultConfig
-	config.DatabaseName = "fhir-test"
-	config.IndexConfigPath = "../fixtures/test_indexes.conf"
+	s.Config = DefaultConfig
+	s.Config.DatabaseName = "fhir-test"
+	s.Config.IndexConfigPath = "../fixtures/test_indexes.conf"
 
 	// Create a temporary directory for the test database
 	var err error
@@ -49,17 +59,14 @@ func (s *MongoIndexesTestSuite) SetupSuite() {
 	s.DBServer = &dbtest.DBServer{}
 	s.DBServer.SetPath("./testdb")
 	s.Session = s.DBServer.Session()
-	s.Database = s.Session.DB(config.DatabaseName)
+	s.Database = s.Session.DB(s.Config.DatabaseName)
 
 	// Set gin to release mode (less verbose output)
 	gin.SetMode(gin.ReleaseMode)
 
 	// Build routes for testing
 	s.Engine = gin.New()
-	RegisterRoutes(s.Engine, make(map[string][]gin.HandlerFunc), NewMongoDataAccessLayer(s.Database, s.Interceptors), config)
-
-	// Configure test indexes
-	ConfigureIndexes(s.Session.Copy(), config)
+	RegisterRoutes(s.Engine, make(map[string][]gin.HandlerFunc), NewMongoDataAccessLayer(s.Database, s.Interceptors), s.Config)
 
 	// Create httptest server
 	s.Server = httptest.NewServer(s.Engine)
@@ -72,7 +79,7 @@ func (s *MongoIndexesTestSuite) TearDownSuite() {
 
 	// remove the temporary database directory
 	var err error
-	err = RemoveContents("./testdb")
+	err = removeContents("./testdb")
 
 	if err != nil {
 		panic(err)
@@ -90,37 +97,155 @@ func TestMongoIndexes(t *testing.T) {
 	suite.Run(t, new(MongoIndexesTestSuite))
 }
 
-func (s *MongoIndexesTestSuite) TestParseIndexes() {
+func (s *MongoIndexesTestSuite) TestParseIndexesStandardIndexAsc() {
 
-	// Test that we're parsing the index names in the config file correctly
-	testIdxConf, err := ioutil.ReadFile("../fixtures/test_indexes.conf")
+	indexStr := "fhir-test.testcollection.foo_1"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(err, "Should return without error")
 
-	if err != nil {
-		panic(err)
-	}
+	indexes, ok := indexMap["testcollection"]
+	s.True(ok, "'testcollection' should be a key in the index map")
+	s.Equal(len(indexes), 1, "indexMap[testcollection] should contain one index")
+	s.Equal(len(indexes[0].Key), 1, "The created index should contain one key")
+	s.Equal(indexes[0].Key[0], "foo", "The index key should be 'foo'")
 
-	allIndexes, err := parseIndexes(string(testIdxConf), "fhir-test")
-
-	s.Nil(err, "The test index configuration file should be parsed without error")
-
-	collectionIndexes, ok := allIndexes["testcollection"]
-	s.Equal(true, ok, "'testcollection' should be a key in the parsed indexes map")
-
-	expectedIndexes := []mgo.Index{
-		mgo.Index{Key: []string{"foo"}},
-		mgo.Index{Key: []string{"-foo"}},
-		mgo.Index{Key: []string{"bar.foo"}},
-		mgo.Index{Key: []string{"-bar.foo"}},
-	}
-
-	s.compareIndexes(expectedIndexes, collectionIndexes)
+	// We only need to check this once, since it's done for all successful indexes
+	s.True(indexes[0].Background, "The index should be set to build in the background")
 }
 
-func (s *MongoIndexesTestSuite) TestIndexesCreated() {
-	var err error
+func (s *MongoIndexesTestSuite) TestParseIndexesStandardIndexDesc() {
+
+	indexStr := "fhir-test.testcollection.foo_-1"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(err, "Should return without error")
+
+	indexes, ok := indexMap["testcollection"]
+	s.True(ok, "'testcollection' should be a key in the index map")
+	s.Equal(len(indexes), 1, "indexMap[testcollection] should contain one index")
+	s.Equal(len(indexes[0].Key), 1, "The created index should contain one key")
+	s.Equal(indexes[0].Key[0], "-foo", "The index key should be '-foo'")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesCompoundIndexAsc() {
+
+	indexStr := "fhir-test.testcollection.(foo_1, bar_1)"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(err, "Should return without error")
+
+	indexes, ok := indexMap["testcollection"]
+	s.True(ok, "'testcollection' should be a key in the index map")
+	s.Equal(len(indexes), 1, "indexMap[testcollection] should contain one index")
+	s.Equal(len(indexes[0].Key), 2, "The created index should contain 2 keys")
+	s.Equal(indexes[0].Key[0], "foo", "The prefix index key should be 'foo'")
+	s.Equal(indexes[0].Key[1], "bar", "The second index key should be 'bar'")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesCompoundIndexDesc() {
+
+	indexStr := "fhir-test.testcollection.(foo_-1, bar_-1)"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(err, "Should return without error")
+
+	indexes, ok := indexMap["testcollection"]
+	s.True(ok, "'testcollection' should be a key in the index map")
+	s.Equal(len(indexes), 1, "indexMap[testcollection] should contain one index")
+	s.Equal(len(indexes[0].Key), 2, "The created index should contain 2 keys")
+	s.Equal(indexes[0].Key[0], "-foo", "The prefix index key should be '-foo'")
+	s.Equal(indexes[0].Key[1], "-bar", "The second index key should be '-bar'")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesCompoundIndexMixed() {
+
+	indexStr := "fhir-test.testcollection.(foo_-1, bar_1)"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(err, "Should return without error")
+
+	indexes, ok := indexMap["testcollection"]
+	s.True(ok, "'testcollection' should be a key in the index map")
+	s.Equal(len(indexes), 1, "indexMap[testcollection] should contain one index")
+	s.Equal(len(indexes[0].Key), 2, "The created index should contain 2 keys")
+	s.Equal(indexes[0].Key[0], "-foo", "The prefix index key should be '-foo'")
+	s.Equal(indexes[0].Key[1], "bar", "The second index key should be 'bar'")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesNoIndexes() {
+
+	indexStr := ""
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	keys := getKeys(indexMap)
+	s.Nil(err, "Should return without error")
+	s.Equal(len(keys), 0, "Should return a map with no indexes in it")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesBadIndexFormat() {
+
+	indexStr := "asdfasdf"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'asdfasdf' is invalid: Not of format <db_name>.<collection_name>.<index(es)>", "Error message should match expected", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesBadDbName() {
+
+	indexStr := "foo.testcollection.(foo_-1, bar_1)"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'foo.testcollection.(foo_-1, bar_1)' is invalid: DB name does not match server configuration", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesNoCollectionName() {
+
+	indexStr := "fhir-test..(foo_-1, bar_1)"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'fhir-test..(foo_-1, bar_1)' is invalid: No collection name given", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesNoKeys() {
+
+	indexStr := "fhir-test.testcollection."
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'fhir-test.testcollection.' is invalid: No index key(s) given", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesBadStandardKeyFormat() {
+
+	indexStr := "fhir-test.testcollection.foo"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'fhir-test.testcollection.foo' is invalid: Standard key not of format: <key>_(-)1", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesBadCompoundKeyFormat() {
+
+	indexStr := "fhir-test.testcollection.(foobar"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'fhir-test.testcollection.(foobar' is invalid: Compound key not of format: (<key1>_(-)1, <key2>_(-)1, ...)", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestParseIndexesBadCompoundKeySubKeyFormat() {
+
+	indexStr := "fhir-test.testcollection.(foo, bar_1)"
+	indexMap, err := parseIndexes(indexStr, "fhir-test")
+	s.Nil(indexMap, "IndexMap should be nil")
+	s.NotNil(err, "Should return an error")
+	s.Equal(err.Error(), "Index 'fhir-test.testcollection.(foo, bar_1)' is invalid: Compound key sub-key not of format: <key>_(-)1", "Unexpected error returned")
+}
+
+func (s *MongoIndexesTestSuite) TestConfigureIndexes() {
+	// Configure test indexes
+	ConfigureIndexes(s.Session.Copy(), s.Config)
 
 	// get the "testcollection" collection. This should have been auto-magically
-	// created the first time the "server" was run
+	// created by ConfigureIndexes
 	c := s.Database.C("testcollection")
 
 	// get the indexes for this collection
@@ -132,14 +257,13 @@ func (s *MongoIndexesTestSuite) TestIndexesCreated() {
 
 	// The indexes *should* be returned in the order they were created.
 	// That's how they're returned by queries in the mongo shell at least.
-	expectedIndexes := []mgo.Index{
-		mgo.Index{Key: []string{"foo"}},
-		mgo.Index{Key: []string{"-foo"}},
-		mgo.Index{Key: []string{"bar.foo"}},
-		mgo.Index{Key: []string{"-bar.foo"}},
-	}
-
 	s.compareIndexes(expectedIndexes, indexes)
+}
+
+func (s *MongoIndexesTestSuite) TestConfigureIndexesNoConfigFile() {
+
+	s.Config.IndexConfigPath = "./does_not_exist.conf"
+	s.NotPanics(func() { ConfigureIndexes(s.Session.Copy(), s.Config) }, "Should not panic if no config file is found")
 }
 
 func (s *MongoIndexesTestSuite) compareIndexes(expected, actual []mgo.Index) {
@@ -168,7 +292,7 @@ func indexInSlice(indexesSlice []mgo.Index, want mgo.Index) bool {
 	return false
 }
 
-func RemoveContents(dir string) error {
+func removeContents(dir string) error {
 	d, err := os.Open(dir)
 	if err != nil {
 		return err
@@ -185,4 +309,16 @@ func RemoveContents(dir string) error {
 		}
 	}
 	return nil
+}
+
+// getKeys returns a slice of all keys in an IndexMap in sorted order
+func getKeys(keyMap IndexMap) []string {
+	keys := make([]string, len(keyMap))
+	var i = 0
+	for k := range keyMap {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	return keys
 }
