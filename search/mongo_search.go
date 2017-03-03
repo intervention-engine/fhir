6adbdf1f5f40001b0ab5eb5808047c484f33b52f
Andre Quina <andre.quina@gmail.com>
2017-03-03 10:38:22 -0500

    fix short circuit on zero record query results. Return the set of empty results instead of nil

--- search/mongo_search.go
+++ search/mongo_search.go
@@ -95,7 +95,7 @@ func (m *MongoSearcher) Search(query Query) (results interface{}, total uint32, 
 
 	// There's no point in running the query if we already know it will return 0 results.
 	if m.readonly && !doCount && total == 0 {
-		return nil, 0, nil
+		return results, 0, nil
 	}
 
 	// execute the query
