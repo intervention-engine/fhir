e7c7d6d091fc296eaa6bbe9ffa6d0ac50b042215
Eugene Lubarsky <eugene@patsoftware.com.au>
2018-05-24 13:05:30 +1000

    Datetime: added support for year-month

--- models/fhirdatetime.go
+++ models/fhirdatetime.go
@@ -9,6 +9,7 @@ type Precision string
 
 const (
 	Date      = "date"
+	YearMonth = "year-month"
 	Timestamp = "timestamp"
 )
 
@@ -21,6 +22,15 @@ func (f *FHIRDateTime) UnmarshalJSON(data []byte) (err error) {
 	if len(data) <= 12 {
 		f.Precision = Precision("date")
 		f.Time, err = time.ParseInLocation("\"2006-01-02\"", string(data), time.Local)
+		if err != nil {
+			f.Precision = Precision("year-month")
+			f.Time, err = time.ParseInLocation("\"2006-01\"", string(data), time.Local)
+		}
+		if err != nil {
+			// TODO: time should go into a separate type
+			// f.Time, err = time.ParseInLocation("\"15:04:05\"", string(data), time.Local)
+		}
+
 	} else {
 		f.Precision = Precision("timestamp")
 		f.Time = time.Time{}
@@ -32,6 +42,8 @@ func (f *FHIRDateTime) UnmarshalJSON(data []byte) (err error) {
 func (f FHIRDateTime) MarshalJSON() ([]byte, error) {
 	if f.Precision == Timestamp {
 		return json.Marshal(f.Time.Format(time.RFC3339))
+	} else if f.Precision == YearMonth {
+		return json.Marshal(f.Time.Format("2006-01"))
 	} else {
 		return json.Marshal(f.Time.Format("2006-01-02"))
 	}
